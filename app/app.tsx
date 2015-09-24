8132a81a2137487df6bd23768f9ff5f60ede6b8e
Kareem Francis <francis.kareem+git@gmail.com>
2015-09-24 00:11:41 -0400

    add example react jsx component

---
+++ app/app.tsx
@@ -0,0 +1,20 @@
+import React = require('react');
+
+class ExampleApplication extends React.Component<{elapsed: number}, {}> {
+    render() {
+        var elapsed = Math.round(this.props.elapsed / 100);
+        var seconds = elapsed / 10 + (elapsed % 10 ? '' : '.0');
+        var message =
+            `React has been successfully running for ${seconds} seconds!!`;
+
+        return <p>{ message } </p>;
+    }
+}
+
+var start = new Date().getTime();
+setInterval(() => {
+    React.render(
+        <ExampleApplication elapsed={ new Date().getTime() - start } />,
+        document.getElementById('main')
+    );
+}, 50);
