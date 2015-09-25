51ffb352332683092505af797f4e815f90a6c1f5
Kareem Francis <francis.kareem+git@gmail.com>
2015-09-25 17:23:28 -0400

    basic matertial design layout - header, content, and footer

--- app/example-component.tsx
+++ app/example-component.tsx
@@ -1,4 +1,7 @@
 import React = require('react');
+import LayoutFooter = require('./layout-footer');
+import LayoutHeader = require('./layout-header');
+
 
 class ExampleApplication extends React.Component<{elapsed: number}, {}> {
     render() {
@@ -8,11 +11,17 @@ class ExampleApplication extends React.Component<{elapsed: number}, {}> {
             `React has been successfully running for ${seconds} seconds!!`;
 
         return(
-            <div>
-                <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-                  Button
-                </button>
-                <p>{ message } </p>
+            <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+                <LayoutHeader/>
+                <main className="mdl-layout__content">
+                    <div className="page-content">
+                        <button className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
+                            Button
+                        </button>
+                        <p>{message}</p>
+                    </div>
+                </main>
+                <LayoutFooter/>
             </div>
         );
     }
---
+++ app/layout-footer.tsx
@@ -0,0 +1,19 @@
+import React = require('react');
+
+class LayoutFooter extends React.Component<{}, {}> {
+    render() {
+        return(
+            <footer className="mdl-mega-footer">
+              <div className="mdl-mega-footer__bottom-section">
+                <div className="mdl-logo">App</div>
+                <ul className="mdl-mega-footer__link-list">
+                  <li><a href="#">Help</a></li>
+                  <li><a href="#">Privacy & Terms</a></li>
+                </ul>
+              </div>
+            </footer>
+        );
+    }
+}
+
+export = LayoutFooter;
